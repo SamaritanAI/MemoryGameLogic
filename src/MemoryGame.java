4399022164661fe0555febc2ca8cce1480904b8a
SamaritanAI <dithipekoketso@gmail.com>
2023-09-27 21:00:41 +0200

    restarted project with best practices

--- src/MemoryGame.java
+++ src/MemoryGame.java
@@ -1,16 +1,16 @@
-//TODO implement loading animations
+//TODO Check user answers in order
+//TODO check user answers in reverse
+//TODO check how many digits the entered were in the list == check which Data structure would be optimal
 
-
-import java.util.Random;
 import java.util.Scanner;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.Random;
 
 public class MemoryGame {
     public static void main(String[] args) {
-        int size = 10, difficulty, upperbound = 10, score = 0;
-        boolean hasWon = false;
-
-        Random rand = new Random();
         Scanner input = new Scanner(System.in);
+        int digits = 10, difficulty = 1;
 
         //setting the difficulty
         System.out.println("""
@@ -22,57 +22,23 @@ public class MemoryGame {
         difficulty = input.nextInt();
 
         if(difficulty == 2) {
-            size = 20;
+            digits = 20;
         }
         else if (difficulty == 3) {
-           size = 30;
+            digits = 30;
         }
-
-        int[] randomNumbers = new int[size];
-        int[] userNumbers = new int[size];
-
-        //store random numbers in array
-        for(int i = 0; i < size; i++) {
-            randomNumbers[i] = rand.nextInt(upperbound) + 1;
-        }
-
-        System.out.println("Number of items: " + size);
-        //output numbers
-        for (int i = 0; i < size; i++) {
-            System.out.print(randomNumbers[i] + "\t");
-        }
-
-        System.out.println("\nTime to insert the numbers.");
-
-        //storing user input
-        for (int j = 0; j < size; j++) {
-
-            String suffix;
-
-            if (j == 0 || j == 20) suffix = "st";
-            else if (j == 1 || j == 21) suffix = "nd";
-            else if (j == 2 || j == 22) suffix = "rd";
-            else suffix = "th";
-
-            System.out.print((j + 1) + suffix + ": ");
-            userNumbers[j] = input.nextInt();
-        }
-
-        //check if user input matches numbers generated
-        for(int i = 0; i < size; i++) {
-            if(userNumbers[i] == randomNumbers[i]) {
-                score++;
-            }
-        }
-
-        if(score >= 8) {
-            hasWon = true;
-            System.out.println("You have gotten " + score + " and have won");
-        }
+        int[] arrReverse = new int[digits];
+        int[] arrAnswers = new  int[digits];
     }
 
-    @Override
-    public String toString() {
-        return "MemoryGame{}";
+    public static int[] randomNumbers(int size) {
+        Random rand = new Random();
+        int[] arrNumbers = new int[size];
+
+        for(int i = 0; i < size; i++) {
+            arrNumbers[i] = rand.nextInt();
+        }
+
+        return arrNumbers;
     }
 }
